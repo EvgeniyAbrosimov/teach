0499d3a19dd25476031b9b3cb6cf6a261507f85b
EvgeniyAbrosimov <aea-metatrans@yandex.ru>
2016-12-29 00:21:41 +0500

    main

--- myapp/views.py
+++ myapp/views.py
@@ -30,3 +30,47 @@ def main(request):
         price_predict = "myapp_camera.price BETWEEN " + chosen_price_min + " AND " + chosen_price_max
     else:
         price_predict = "myapp_camera.price BETWEEN 0 AND " + chosen_price_max
+
+        # --- формируем строку условия(бренд) на языке sql для запроса к БД ---
+    chosen_brand = ""
+    try:
+        chosen_brand = request.POST['brand_checkbox']
+    except MultiValueDictKeyError:
+        pass
+
+    # ПРОВЕРКА на SQl-иньекцию
+    if (chosen_brand.replace(" ", "")).isalpha():
+        if (chosen_brand != ""):
+            brand_predict = " AND myapp_brand.name = '" + chosen_brand + "'"
+        else:
+            brand_predict = ""
+    else:
+        brand_predict = ""
+
+        # --- итоговое условие выборки товаров из БД для оператора WHERE
+    where_predict = price_predict + \
+                    brand_predict
+
+    # массив выбранных видеокамер
+    chosen_cameras = []
+    # запрос выборки видеокамер из БД, по условию
+    for cam in Camera.objects.raw('''SELECT myapp_camera.id,
+            myapp_camera.name AS name,
+            myapp_brand.name AS prod_brand,
+            myapp_camera.description AS description,
+            myapp_camera.page AS page,
+            myapp_camera.price AS price
+            FROM myapp_camera
+            JOIN myapp_brand ON myapp_camera.brand_id = myapp_brand.id
+            WHERE ''' + where_predict +
+                                          ''' ORDER BY price'''):
+        chosen_cameras.append([cam.image,
+                               cam.name,
+                               cam.prod_brand,
+                               cam.price,
+                               cam.page,
+                               cam.description])
+
+    return render(request, 'myapp/main.html', {'chosen_cameras': chosen_cameras,
+                                               'brands': brands,
+                                               'max_price': max_price})
